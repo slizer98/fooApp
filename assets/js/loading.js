3ca7999f892a8909acbb3736924d616d6e967b6d
Erick Villafuerte <erick.m.villa.r@gmail.com>
2022-01-29 19:14:41 -0600

    arreglando index

---
+++ assets/js/loading.js
@@ -0,0 +1,25 @@
+// Variables
+const loading = document.getElementById('loading');
+
+// Event listeners
+eventListeners();
+function eventListeners(){
+    document.addEventListener('DOMContentLoaded', cargarLoading);
+}
+
+// Funciones
+function cargarLoading(){
+    const logo = document.createElement('div');
+    logo.classList.add('logo')
+    const imgLogo = document.createElement('img');
+    imgLogo.src = '/assets/images/logo.svg'
+    logo.appendChild(imgLogo)
+    loading.appendChild(logo);
+
+    const fruits = document.createElement('div');
+    fruits.classList.add('fruits')
+    const imgFruit = document.createElement('img');
+    imgFruit.src = '/assets/images/fruits.svg'
+    fruits.appendChild(imgFruit)
+    loading.appendChild(fruits);
+}
